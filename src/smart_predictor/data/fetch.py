989778a55fdc8fc5c2e737358a5de2215136528b
Julio Antonio Megu <julioamdads@gmail.com>
2026-01-18 00:47:02 +0700

    add files and folders

---
+++ src/smart_predictor/data/fetch.py
@@ -0,0 +1,9 @@
+import requests 
+from python-dotenv
+
+eia_url = "https://api.eia.gov/v2/electricity/rto/region-data/data/?frequency=hourly&data[0]=value&start=2024-01-01T00&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+
+response = requests.get(eia_url)
+
+# Check if the 
+
